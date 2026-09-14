2b34fce528beb91882d17c1bd870da5f9b929dbd
ukrbanditos-svg <ukrbanditos@gmail.com>
2026-09-14 18:23:22 -0400

    bn
    b v

--- SET_DOMAIN.bat
+++ SET_DOMAIN.bat
@@ -1,8 +1,4 @@
 @echo off
-chcp 65001 >nul
-set /p DOMAIN=Вставь адрес сайта без слэша в конце (пример: https://dey6.ru): 
-if "%DOMAIN%"=="" exit /b 1
-powershell -NoProfile -ExecutionPolicy Bypass -Command "$old='https://YOUR-DOMAIN.example'; $new='%DOMAIN%'; Get-ChildItem -Recurse -File | Where-Object { $_.Extension -in '.html','.xml','.txt','.webmanifest' -or $_.Name -eq 'manifest.webmanifest' } | ForEach-Object { $p=$_.FullName; $c=[IO.File]::ReadAllText($p); if($c.Contains($old)){ [IO.File]::WriteAllText($p,$c.Replace($old,$new),(New-Object Text.UTF8Encoding($false))) } }"
-echo.
-echo Готово. Canonical, Open Graph, sitemap и RSS настроены на %DOMAIN%
+echo Site is already configured for https://dey6.online
+echo No domain replacement is required.
 pause
